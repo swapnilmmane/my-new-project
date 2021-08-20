e5e02c5258cec71a121110d52a8ba53dfd36dbbf
Swapnil M Mane <swapnilmmane@gmail.com>
2021-08-20 15:07:46 +0530

    Extend Admin Aria with Car Manufacturers

--- api/code/graphql/src/plugins/scaffolds/index.ts
+++ api/code/graphql/src/plugins/scaffolds/index.ts
@@ -1,3 +1,4 @@
 // This file is automatically updated via various scaffolding utilities.
+import carManufacturers from "./carManufacturers";
 
-export default () => [];
+export default () => [carManufacturers];
